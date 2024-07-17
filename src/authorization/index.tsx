c83e30bfae0eca76dbccf12424d9fb5dc2d45469
monsterdev89 <quansera.dane@gmail.com>
2024-07-18 00:34:01 +0900

    auth

--- src/authorization/index.tsx
+++ src/authorization/index.tsx
@@ -6,45 +6,21 @@
 import axios from 'axios';
 import { Buffer } from 'buffer';
 import { signTypedData, SignTypedDataVersion, TypedMessage } from '@metamask/eth-sig-util';
-// import sigUtil from '@metamask/eth-sig-util';
-
-// type MessageTypes = {
-//     EIP712Domain: [
-//         { name: 'name'; type: 'string' },
-//         { name: 'chainId'; type: 'uint256' },
-//         { name: 'version'; type: 'string' },
-//         { name: 'salt'; type: 'bytes32' }
-//     ];
-//     Challenge: [
-//         { name: 'address'; type: 'address' },
-//         { name: 'nonce'; type: 'string' },
-//         { name: 'message'; type: 'string' }
-//     ];
-// };
-
-
 
 export const Authorization = async () => {
     const domain = "https://api.inftytrade.xyz";
-
 
     const address = "0x8E71fbF0f1a49ed1a3c774Bf7477dc39f66D268f";
     const hexKey = "fc8653d8a1e564334aa428521de4657310364c300f3f0193044e7ed1f413e94a";
     const chain = "b_square_testnet";
     const walletType = "metamask";
-    console.log("Hello, World!");
 
     const resp = await axios.get(`${domain}/v1/auth/login?chain=${chain}&address=${address}`);
-    console.log("response => ", resp.data);
 
     const privateKey = loadPrivateKey(hexKey);
-    console.log(privateKey.toString('hex'));
-
-    // const typedData: TypedMessage<MessageTypes> = resp.data.data.sign_data;
     const typedData: any = resp.data.data.sign_data;
 
     const signature = signTypedDataTS(privateKey, typedData);
-    console.log("Signature:", signature);
 
     const body = {
         address: address,
@@ -56,8 +32,6 @@ export const Authorization = async () => {
     const resp2 = await axios.post(`${domain}/v1/auth/login`, body, {
         headers: { 'Content-Type': 'application/json' }
     });
-
-    console.log("access token response:", resp2.data);
 
     return resp2.data
 }
