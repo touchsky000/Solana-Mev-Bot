f3159fd8c5a69afc738d598ee31c277b0d4a4bec
monsterdev89 <quansera.dane@gmail.com>
2024-09-14 01:13:12 +0900

    feature: position close function is added

--- src/components/table/ordersCard.tsx
+++ src/components/table/ordersCard.tsx
@@ -59,50 +59,48 @@ export default function OrdersCard(props: any) {
   }, [props])
 
   return (
-    <>
-      <div className="w-full p-7">
-        <div className="sm:flex sm:justify-start items-baseline gap-10 grid grid-cols-2">
-          <h2 className="font-medium sm:text-2xl text-xl">{orders.market_display}</h2>
-          <p className={`font-medium sm:text-xl text-base ${orders.side === "short" ? "text-rose-500" : "text-semantic-success"}`}>
-            {orders.side === "short" ? "Short" : "Long"}
+    <div className="w-full p-7">
+      <div className="sm:flex sm:justify-start items-baseline gap-10 grid grid-cols-2">
+        <h2 className="font-medium sm:text-2xl text-xl">{orders.market_display}</h2>
+        <p className={`font-medium sm:text-xl text-base ${orders.side === "short" ? "text-rose-500" : "text-semantic-success"}`}>
+          {orders.side === "short" ? "Short" : "Long"}
+        </p>
+      </div>
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-3 py-3">
+        <div className="">
+          <p className="text-text-secondary text-lg">
+            Size <span className="block sm:inline text-white">{toWei(orders.size).toFixed(6)} BTC</span>
           </p>
         </div>
-        <div className="grid grid-cols-2 lg:grid-cols-3 gap-3 py-3">
-          <div className="">
-            <p className="text-text-secondary text-lg">
-              Size <span className="block sm:inline text-white">{toWei(orders.size).toFixed(6)} BTC</span>
-            </p>
-          </div>
-          <div>
-            <p className="text-text-secondary text-lg">
-              Order Price{" "}
-              <span className="block sm:inline text-white">{toNum(orders.trigger_price).toFixed(2)}</span>
-            </p>
-          </div>
+        <div>
+          <p className="text-text-secondary text-lg">
+            Order Price{" "}
+            <span className="block sm:inline text-white">{toNum(orders.trigger_price).toFixed(2)}</span>
+          </p>
+        </div>
 
-          <div className="flex col-span-full justify-end gap-2 order-last lg:order-none lg:col-span-1 lg:justify-start">
-            <button
-              className="px-4 py-1 rounded-full border w-[150px]"
-              onClick={() => { cancelIncreaseOrder() }}
-            >
-              {!isAnimation && "Cancel"}
-              {
-                isAnimation &&
-                <div className="w-[100%] h-[100%] stage">
-                  <div className='dot-typing'>
-                  </div>
+        <div className="flex col-span-full justify-end gap-2 order-last lg:order-none lg:col-span-1 lg:justify-start">
+          <button
+            className="px-4 py-1 rounded-full border w-[150px]"
+            onClick={() => { cancelIncreaseOrder() }}
+          >
+            {!isAnimation && "Cancel"}
+            {
+              isAnimation &&
+              <div className="w-[100%] h-[100%] stage">
+                <div className='dot-typing'>
                 </div>
-              }
-            </button>
-          </div>
-          <div>
-            <p className="text-text-secondary text-lg">
-              Order Size{" "}
-              <span className="block sm:inline text-white">{(toNum(orders.trigger_price) * toWei(orders.size)).toFixed(2)} USDC</span>
-            </p>
-          </div>
+              </div>
+            }
+          </button>
+        </div>
+        <div>
+          <p className="text-text-secondary text-lg">
+            Order Size{" "}
+            <span className="block sm:inline text-white">{(toNum(orders.trigger_price) * toWei(orders.size)).toFixed(2)} USDC</span>
+          </p>
         </div>
       </div>
-    </>
+    </div>
   );
 }
